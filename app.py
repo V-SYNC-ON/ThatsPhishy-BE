034b19b05c3c45a717e1fbd56d04261c73482ac6
GREEDnim <sivadhas007@gmail.com>
2023-09-22 22:59:10 +0530

    feat(database) : add MongoDB connection and caching

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
-
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-
+from pymongo import MongoClient
+from decouple import config
 from utils.ModelUtils import get_prediction
-from utils.URLUtils import get_description, get_domain, get_status, reformat_url, url_exists
+from utils.URLUtils import get_description, get_domain, get_status, reformat_url, url_exists,is_mongodb_alive
 
 app = Flask(__name__)
 CORS(app)
+mongo_uri = config('MONGO_URI')
+client = MongoClient(mongo_uri)
+db = client['thatsPhisy'] 
+collection = db['cache']  
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,21 +21,29 @@ def predict():
         url=reformat_url(url)
         if(url_exists(url)==False) :
             return make_response(jsonify({"error": "The URL doesn't exist "}), 400)
-
-        output=get_prediction(url)
         domain_name=get_domain(url)
-        status=get_status(output)
-        description=get_description(output,url)
-        response = {
-            'prediction': output,
-            'domain':domain_name,
-            'status':status,
-            'description':description
-        }
-        return make_response(jsonify(response),200)
+        response = {}
+        response = collection.find_one({'domain': domain_name})
+        if not response:
+            output = get_prediction(url)
+            status = get_status(output)
+            description = get_description(output, url)
+            response = {
+                'prediction': output,
+                'domain': domain_name,
+                'status': status,
+                'description': description
+            }
+            collection.insert_one(response)
+        response.pop('_id', None)
+        return make_response(jsonify(response), 200)
+    
     except Exception as e:
         return make_response(jsonify({'Error': 'Something Went Wrong'}), 500)
-
+    
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
+    if(is_mongodb_alive(client)==False):
+        print("error connecting to db")
+        exit(1)
     app.run(host='0.0.0.0', port=port)
